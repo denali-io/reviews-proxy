f814d6738d67961d7f2f6e3d992cd54fcd4f819e
Grant Belcher <gtbelch@gmail.com>
2020-03-23 22:55:02 -0700

    sets up proxy server to render Reviews Component

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
+const httpProxy = require('http-proxy');
 const port = 2000;
 const app = express();
+var proxy = httpProxy.createProxyServer({});
+app.use(cors())
+app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('/', (req, res) => {
-    res.send('Hello from Express!')
+app.all('/', (req, res) => {
+    console.log('req')
+    proxy.web(req, res, {target: 'http://localhost:5000'})
 })
+
 
 app.listen(port, () => {
     console.log('PROXY listening on port 2000')
